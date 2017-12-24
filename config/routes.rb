dff4b045752108446efea16c9e65d8c44846e28a
alpbourne <alpbourne@gmail.com>
2017-12-24 16:36:10 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   root 'welcome#home'
   resources :users
   resources :attractions
+  resources :sessions, only: [:destroy, :create, :new]
 
   get "/signin", to: "sessions#new"
   post "/sessions/create", to: "sessions#create"
-  get "/signout", to: "sessions#destroy"
+  delete "/signout", to: "sessions#destroy"
   post "/rides/new", to: "rides#new"
 end
