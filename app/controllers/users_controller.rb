922b8623711a11478a139a6d4ded51749d7e4a05
alpbourne <alpbourne@gmail.com>
2017-12-24 16:25:36 -0500

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to '/'
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    if session[:user_id]
+      @user = User.find(params[:id])
+    else
+      redirect_to '/'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :happiness, :nausea, :height, :tickets, :admin)
+  end
+
+end
